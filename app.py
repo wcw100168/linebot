0eab49b5f5b1d9bfca4da90837b98f2b4e4e7660
wcw100168 <119844585+wcw100168@users.noreply.github.com>
2023-08-20 00:49:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,8 +54,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    GPT_answer = GPT_response(msg)
     print(msg)
+    
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    GPT_answer = GPT_response(msg)
     print(GPT_answer)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
 
