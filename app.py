6dc58bb4462559ddc01bf8a27dcc0dfa36430fce
wcw100168 <119844585+wcw100168@users.noreply.github.com>
2023-08-20 20:07:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,9 @@ def handle_message(event):
         img_url='https://megapx-assets.dcard.tw/images/12d3fe91-e447-44f7-9086-6ec1642b9656/full.jpeg'
         img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
         line_bot_api.reply_message(event.reply_token,img_message)
+    if '行事曆' in msg:
+        message = TextSendMessage(text='https://www.nchu.edu.tw/calendar/')
+        line_bot_api.reply_message(event.reply_token, message)
     if '新生EZ come' in msg:
         message = TextSendMessage(text='單一簽證入口：\n''https://portal.nchu.edu.tw/portal/\n''帳號：學號\n''密碼：身分證開頭字母大小寫＋身分證後4碼＋生日4碼\n\n''左側選單即有入口')
         line_bot_api.reply_message(event.reply_token, message)
