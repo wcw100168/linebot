e34758656fd5bbff134a4a4fd4eb2ae0029d24f6
wcw100168 <119844585+wcw100168@users.noreply.github.com>
2023-08-20 18:30:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def handle_message(event):
         img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
         line_bot_api.reply_message(event.reply_token,img_message)
     if '新生EZ come' in msg:
-        message = TextSendMessage(text='https://onepiece2-sso.nchu.edu.tw/cofsys/plsql/ezcome_main?p_init=Y')
+        message = TextSendMessage(text='單一簽證入口：''https://portal.nchu.edu.tw/portal/')
         line_bot_api.reply_message(event.reply_token, message)
     else:
         message = TextSendMessage(text='收到')
