d6f70905d79f7e876e2af510926d81dbefebbf04
wcw100168 <119844585+wcw100168@users.noreply.github.com>
2023-08-20 15:26:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,13 +25,13 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
-def GPT_response(text):
-    # 接收回應
-    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
-    print(response)
-    # 重組回應
-    answer = response['choices'][0]['text'].replace('。','')
-    return answer
+#def GPT_response(text):
+#    # 接收回應
+#    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
+#    print(response)
+#    # 重組回應
+#    answer = response['choices'][0]['text'].replace('。','')
+#    return answer
 
 
 # 監聽所有來自 /callback 的 Post Request
