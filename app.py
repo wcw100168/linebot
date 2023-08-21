50e82d8521e3171e7bc947c71b3758211a9422b2
wcw100168 <119844585+wcw100168@users.noreply.github.com>
2023-08-21 14:57:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -6,12 +7,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+
 #======python的函數庫==========
 import tempfile, os
 import datetime
 import openai
 import time
+import random
 #======python的函數庫==========
+
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
@@ -20,6 +24,8 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
 openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
 #def GPT_response(text):
 #    # 接收回應
 #    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
@@ -27,6 +33,8 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 #    # 重組回應
 #    answer = response['choices'][0]['text'].replace('。','')
 #    return answer
+
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -41,28 +49,35 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
+
+img_urls = ['https://megapx-assets.dcard.tw/images/12d3fe91-e447-44f7-9086-6ec1642b9656/full.jpeg',
+           'https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1604631271916.jpg',
+           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZFjmvS6JMNuUoPidFc3MvaOXKHP78IA7kLA&usqp=CAU']
+
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    random_image_url = random.choice(image_urls)
+    image_message = ImageSendMessage(original_content_url=random_image_url,preview_image_url=random_image_url)
     msg = event.message.text
     if '你好' in msg:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='我不好'))
     if '請問' in msg:
-        img_url='https://megapx-assets.dcard.tw/images/12d3fe91-e447-44f7-9086-6ec1642b9656/full.jpeg'
+        num=random.randint(0,3)
+        img_url=reply_img(num)
         img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
         line_bot_api.reply_message(event.reply_token,img_message)
     if '行事曆' in msg:
         message = TextSendMessage(text='https://www.nchu.edu.tw/calendar/')
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, (image_message,message))
     if '推薦排課系統' in msg:
         message = TextSendMessage(text='https://nchuclass.axisflow.biz/Login')
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, (image_message,message))
     if '直屬' in msg:
         message = TextSendMessage(text='要看係，有些系沒有，通常是和你同號碼的學長')
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, (image_message,message))
     if '選課推薦' in msg:
-        img_url='https://megapx-assets.dcard.tw/images/12d3fe91-e447-44f7-9086-6ec1642b9656/full.jpeg'
-        img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
         message = TextSendMessage(text='請善用網路\n''https://www.dcard.tw/search?query=%E9%81%B8%E8%AA%B2&forum=nchu')
         line_bot_api.reply_message(event.reply_token, [img_message ,message] )
     if '新生EZ come' in msg:
@@ -71,19 +86,23 @@ def handle_message(event):
                                   '🎈帳號：學號\n'
                                   '🎈密碼：身分證開頭字母大小寫＋身分證後4碼＋生日4碼\n\n'
                                   '🎈左側選單即有入口')
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, (image_message,message))
     if '公車' in msg:
         message = TextSendMessage(text='https://www.dcard.tw/f/nchu/p/252906645')
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, (image_message,message))
     else:
         message = TextSendMessage(text='收到')
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(event.reply_token, (image_message,message))
 #        GPT_answer = GPT_response(msg)
 #        print(GPT_answer)
 #        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+
+
 @handler.add(PostbackEvent)
 def handle_message(event):
     print(event.postback.data)
+
+
 @handler.add(MemberJoinedEvent)
 def welcome(event):
     uid = event.joined.members[0].user_id
